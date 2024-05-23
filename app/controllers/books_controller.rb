638c40beccaca5755150c4a97f015b359bb53a64
EC2 Default User <ec2-user@ip-172-31-36-164.ap-northeast-1.compute.internal>
2024-05-23 11:14:30 +0000

    [lest]0522-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,20 +7,23 @@ class BooksController < ApplicationController
   def create
      @book = Book.new(book_params)
      @book.user_id = current_user.id
+     @user=current_user
+     @books=Book.all
     if @book.save
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id) 
-    # リダイレクト先は
+    
     else
-      render:new
+      # @books = Book.all
+      render:index
     end
   end
   
   def index
     @books = Book.all 
-    # @book=Book.new
+    # @book = Book.new(book_params)
+    @book=Book.new
     @user=current_user
-    # @user = @user.profile_imageこれいらんけど保留   
   end
   
   def show
